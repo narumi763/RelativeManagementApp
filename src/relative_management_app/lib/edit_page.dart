1ca9c718919d7d98542f25f0ffbc486aed6ed734
narumi763 <nk.web.div@gmail.com>
2022-10-15 17:24:51 +0900

    追加画面のコントロール追加

--- src/relative_management_app/lib/edit_page.dart
+++ src/relative_management_app/lib/edit_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class EditPage extends StatelessWidget{
+class EditPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _editPageState();
+}
+
+class _editPageState extends State<EditPage>
+{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,14 +15,52 @@ class EditPage extends StatelessWidget{
         title: Text('顔写真管理 - 追加'),
       ),
       body:Center(
-        child: ElevatedButton(
-          child: const Text('戻る'),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
+        child: Column(
+          children: [
+            Text("名前"),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: '例）田中　太郎'
+              ),
+            ),
+            Text("メモ"),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: '例）いとこ'
+              ),
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                OutlinedButton(
+                  child: const Text('キャンセル'),
+                  style: TextButton.styleFrom(
+                    primary: Colors.black,
+                  ),
+                  onPressed: (){},
+                ),
+                ElevatedButton(                  
+                  child: const Text("登録"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )
+                  ),
+                  onPressed: (){},
+                ),                
+              ],
+            )
+          ],
+        )
       )
     );
   }
-  
+
 }
